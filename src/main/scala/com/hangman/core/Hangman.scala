0b20b13b2c46fbc6ebe1cb470cfcf49f69ecdf19
Ainur Hakimov <techlinked@yandex.ru>
2021-07-28 23:19:48 +0500

    Hangman implementation

--- src/main/scala/com/hangman/core/Hangman.scala
+++ src/main/scala/com/hangman/core/Hangman.scala
@@ -1,6 +1,7 @@
 package com.hangman.core
 
 import cats.Monad
+import cats.syntax.all._
 import com.hangman.core.Hangman.{GuessResult, State}
 import com.hangman.dictionary.DictionaryService
 import com.hangman.interface.UserInterface
@@ -34,17 +35,42 @@ class Hangman[F[_]: Monad](
   dictionaryService: DictionaryService[F])
 {
 
-  def getChoice: F[Char] = ???
+  def getChoice: F[Char] = ui.getChar.map(_.toLower)
 
-  def renderState(state: State): F[Unit] = ???
+  def renderState(state: State): F[Unit] = {
+    val maskedWord = state.word.map(c => if (state.guesses.contains(c)) c else '*')
+    ui.print(s"Mistakes: ${state.failures} of ${state.maxFailures}")
+    ui.print(s"The word: $maskedWord")
+  }
 
   def analyzeNewInput(
     oldState: State,
     newState: State,
     char: Char
-  ): GuessResult = ???
+  ): GuessResult = {
+    if (oldState.guesses.contains(char)) GuessResult.Unchanged
+    else if (newState.playerWon) GuessResult.Won
+    else if (newState.playerLost) GuessResult.Lost
+    else if (oldState.word.contains(char)) GuessResult.Correct
+    else GuessResult.Incorrect
+  }
 
-  def gameLoop(oldState: State): F[Unit] = ???
+  def gameLoop(oldState: State): F[Unit] = for {
+    _ <- ui.print(s"Guess a letter:")
+    c <- getChoice
+    currentState = oldState.copy().addChar(c)
+    guessResult = analyzeNewInput(oldState, currentState, c)
+    _ <- renderState(currentState)
+    _ <- guessResult match {
+      case GuessResult.Won => ui.print(s"You won!")
+      case GuessResult.Lost => ui.print(s"You lost!")
+      case _ => gameLoop(currentState)
+    }
+  } yield ()
 
-  def startGame(): F[Unit] = ???
+  def startGame(): F[Unit] = for {
+    _ <- ui.print("\n -- New game! --")
+    word <- dictionaryService.getWord
+    _ <- gameLoop(Hangman.State(Set(), word))
+  } yield ()
 }
--- src/main/scala/com/hangman/dictionary/interpreter/SimpleDictionaryService.scala
+++ src/main/scala/com/hangman/dictionary/interpreter/SimpleDictionaryService.scala
@@ -1,10 +1,27 @@
 package com.hangman.dictionary.interpreter
 
 import cats.Monad
+import cats.syntax.all._
 import com.hangman.dictionary.DictionaryService
+import com.hangman.dictionary.interpreter.SimpleDictionaryService.dictionary
+
+import scala.util.Random
 
 class SimpleDictionaryService[F[_]: Monad] extends DictionaryService[F] {
 
-  override def getWord: F[String] = ???
+  override def getWord: F[String] = for {
+    randomIndex <- Random.nextInt(dictionary.length).pure[F]
+    word <- dictionary(randomIndex).pure[F]
+  } yield word
+}
+
+private[dictionary] object SimpleDictionaryService {
+
+  val dictionary = List(
+    "hello",
+    "my",
+    "little",
+    "pony"
+  )
 }
 
